46794a6b028d650b68fb28b61b1d9dadfde8f0ba
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-03-26 22:51:31 +0300

    [fix] browser api view is now just a json

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
 
 # CORS
