cd51bf2a819e4384044d56d6bfba3ed559f39081
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-03-26 20:29:57 +0300

    [add] swagger

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Chameleon API",
+        default_version="v1",
+        description="Description",
+        contact=openapi.Contact(email="evlko.oklve@gmail.com"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("apps.interface_flows_api.urls")),
+    path(
+        "docs/swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
