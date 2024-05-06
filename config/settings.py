5ce3034de304d3a86af56c2df254a0ac91e146a6
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-05-06 12:32:42 +0300

    [remove] unlimit DATA_UPLOAD_MAX_NUMBER_FILES

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DEBUG_MODE")))
 
-DATA_UPLOAD_MAX_NUMBER_FILES = None
-
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
